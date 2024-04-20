8eed9800e97fa8590def4fee314736f543935bc6
Bilal <taybib4@gmail.com>
2024-04-20 01:22:06 +0100

    products crud

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const cors = require("cors");
 
 const auth = require("./routes/auth");
+const productModel = require("./models/product.model");
 const PORT = 8080;
 const encodedPassword = encodeURIComponent(process.env.MONGO_PWD);
 const BASE_MONGO_URI = `mongodb+srv://${process.env.MONGO_USERNAME}:${encodedPassword}`;
@@ -16,6 +17,65 @@ app.use(
     extended: true,
   })
 );
+
+// create products
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = productModel.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {}
+});
+
+// get products
+app.get("/api/products", async (req, res) => {
+  try {
+    const product = await productModel.find({});
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// get one products
+app.get("/api/product/:id", async (req, res) => {
+  try {
+    const product = await productModel.findById(req.params.id);
+
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// update product
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const product = await productModel.findByIdAndUpdate(
+      req.params.id,
+      req.body
+    );
+
+    if (!product) return res.status(404).json({ message: "product not found" });
+
+    const updatedProduct = await productModel.findById(req.params.id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// delete product
+app.delete("/api/product/:id", async (req, res) => {
+  try {
+    const product = await productModel.findByIdAndDelete(req.params.id);
+
+    if (!product) return res.status(404).json({ message: "product not found" });
+
+    res.status(200).json({ message: "product deleted" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 mongoose.connect(`${BASE_MONGO_URI}${process.env.MONGO_URI}`).then(() => {
   console.log("CONNECTED");
